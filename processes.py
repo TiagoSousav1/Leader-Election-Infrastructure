671dd67c8a78b5e5090da2d88e12a23097cce86a
Tiago Sousa laptop <up201907205@fe.up.pt>
2024-12-11 00:40:10 +0000

    INITIAL COMMIT

---
+++ processes.py
@@ -0,0 +1,86 @@
+import multiprocessing
+import os
+import socketserver
+import socket
+
+MSGLEN = 10
+
+class MySocket:
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+
+def worker1():
+    # printing process id
+    print("ID of process running worker1: {}".format(os.getpid()))
+    socket = MySocket()
+    socket.mysend(b"Sending msg")
+    print(socket.sock)
+    while(1):
+        pass
+
+def worker2():
+    # printing process id
+    print("ID of process running worker2: {}".format(os.getpid()))
+    socket = MySocket()
+    socket.myreceive()
+    print(socket.sock)
+    while(1):
+        pass
+
+if __name__ == "__main__":
+    # printing main program process id
+    print("ID of main process: {}".format(os.getpid()))
+
+    # creating processes
+    p1 = multiprocessing.Process(target=worker1)
+    p2 = multiprocessing.Process(target=worker2)
+
+    # starting processes
+    p1.start()
+    p2.start()
+
+    # process IDs
+    print("ID of process p1: {}".format(p1.pid))
+    print("ID of process p2: {}".format(p2.pid))
+
+    # wait until processes are finished
+    p1.join()
+    p2.join()
+
+    # both processes finished
+    print("Both processes finished execution!")
+
+    # check if processes are alive
+    print("Process p1 is alive: {}".format(p1.is_alive()))
+    print("Process p2 is alive: {}".format(p2.is_alive()))
